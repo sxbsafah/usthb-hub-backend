34cda30e4200890f6a0b48c601e6dadb3910d77f
Khodjabache Mhamed <sxbsafah@gmail.com>
2025-12-22 23:19:27 +0100

    fixing a bug

--- src/lib/facultyName.ts
+++ src/lib/facultyName.ts
@@ -1,6 +1,9 @@
 import Faculty from "@/models/faculty";
+import mongoose from "mongoose";
 
-export async function getFacultyNameById(facultyId) {
+export async function getFacultyNameById(
+  facultyId: mongoose.Types.ObjectId | string
+) {
   if (!facultyId) return null;
   const faculty = await Faculty.findById(facultyId).select("name");
   return faculty ? faculty.name : null;
